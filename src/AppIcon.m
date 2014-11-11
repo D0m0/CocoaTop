084d3b955da422f0de8e1ada7d38762dfd216288
Domo <domo@linux.com>
2014-11-12 00:25:56 +0300

    Testing AppList API

--- src/AppIcon.m
+++ src/AppIcon.m
@@ -1,4 +1,5 @@
 #import <Foundation/NSPathUtilities.h>
+//#import <AppList/ALApplicationList.h>
 #import "AppIcon.h"
 
 @implementation PSAppIcon
@@ -19,6 +20,9 @@
 //		self.bundleName = [app valueForKey:@"CFBundleName"];
 //		self.displayName = [app valueForKey:@"CFBundleDisplayName"];
 		self.path = [app valueForKey:@"Path"];
+
+		NSLog(@"self.name = %@", self.name);
+		NSLog(@"self.path = %@", self.path);
 
 		NSMutableArray *IconNames = [NSMutableArray arrayWithObjects:@"Icon", @"icon", @"Icon-Small", @"Icon-Small-50", @"Icon-Small-40", @"icon-114", nil];
 		for (NSString *Key in [NSArray arrayWithObjects:@"CFBundleIcons", @"CFBundleIconFiles", @"CFBundleIconFile", nil]) {
@@ -56,28 +60,68 @@
 
 + (NSArray *)psAppIconArray
 {
-	NSMutableArray *AppIconArray = [NSMutableArray array];
+//	NSMutableArray *AppIconArray = [NSMutableArray array];
+
+	// list all the apps
+	ALApplicationList *apps = [ALApplicationList sharedApplicationList];
+ 
+	// sort the apps by display name. displayIdentifiers is an autoreleased object.
+	NSArray *displayIdentifiers = [apps.applications allKeys];
+//	[[apps.applications allKeys] sortedArrayUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+//		return [[apps.applications objectForKey:obj1] caseInsensitiveCompare:[apps.applications objectForKey:obj2]];
+//	}];
+/*	for (NSString *key in displayIdentifiers) {
+//		id app = [apps.applications objectForKey:key];
+//		NSString *path = NSStringFromClass([app class]);//[app objectForKey:@"path"];
+//		NSDictionary *app = [apps.applications objectForKey:key];
+		NSLog(@"%@", key);
+		//id app = [apps valueForKey:@"path" forDisplayIdentifier:[apps.applications objectForKey:key]];
+		//SBApplication *app = [SBApplication applicationWithBundleIdentifier:key];
+		SBApplication *app = [[SBApplicationController sharedInstance] applicationWithDisplayIdentifier:key];
+		NSLog(@"App: %@", app);
+//		NSBundle *myBundle = [NSBundle bundleWithIdentifier:key];
+//		NSLog(@"Path: %@", [myBundle bundlePath]);
+//		NSLog(@"Dict: %@", [myBundle infoDictionary]);
+	}
+*/
+	Class SBApplicationController = objc_getClass("SBApplicationController");
+NSLog(@"SBApplicationController: %@", SBApplicationController);
+	Class SBApplication = objc_getClass("SBApplication");
+NSLog(@"SBApplication: %@", SBApplication);
+	id controller = [SBApplicationController sharedInstance];
+NSLog(@"ctrlid: %@", controller);
+	for (NSString *appId in [controller allBundleIdentifiers]) { 
+		NSLog (@"bundle: %@", appId);
+//		NSLog ([NSString stringWithFormat:@"bundle: %@", appId]);
+		NSArray *apps = [controller applicationsWithBundleIdentifier:appId];
+		if ([apps count] > 0) { 
+			id app = [apps objectAtIndex:0]; 
+//			[self indexApp:app withName:[app displayName]];
+			NSLog(@"App: %@", [app displayName]);
+		}
+	}
+	return displayIdentifiers;
 //	@autoreleasepool {
-		NSDictionary *DicFile = [NSDictionary dictionaryWithContentsOfFile:
+/*		NSDictionary *DicFile = [NSDictionary dictionaryWithContentsOfFile:
 			[[self psGetAppInfoPath] stringByAppendingPathComponent:@"com.apple.mobile.installation.plist"]];
 		NSDictionary *DicKeySyst = [DicFile objectForKey:@"System"];
-/*		if (DicKeySyst)//[DicKeySyst.allKeys respondsToSelector:@selector(objectForKey)])
+		if (DicKeySyst)//[DicKeySyst.allKeys respondsToSelector:@selector(objectForKey)])
 		{
 			UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"OK" message:NSStringFromClass([DicKeySyst class])
 				delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
 			[alertView show];
 		}
-*/		if (DicKeySyst)
+		if (DicKeySyst)
 		for (id App in DicKeySyst)
 			[AppIconArray addObject:[PSAppIcon psAppIconWithAppKey:[DicKeySyst objectForKey:App]]];
-/*		NSDictionary *DicKeyUser = [DicFile objectForKey:@"User"];
+		NSDictionary *DicKeyUser = [DicFile objectForKey:@"User"];
 		for (NSDictionary *App in DicKeyUser)
 			[AppIconArray addObject:[PSAppIcon psAppIconWithAppKey:App]];
 //		AppInfoSorted = [AppInfoArray sortedArrayUsingFunction:alphabeticAppInfoSort context:Context];
 //		self.AppInformationArray = AppInfoSorted;
 */
 //	}
-	return AppIconArray;
+//	return AppIconArray;
 }
 
 + (UIImage *)roundCorneredImage:(UIImage *)orig size:(NSInteger)dim radius:(CGFloat)r
@@ -93,10 +137,11 @@
 
 + (NSString *)getIconFileFromArray:(NSArray *)appIconArray forApp:(NSString *)fullPath
 {
-	NSString *appPath = [fullPath stringByDeletingLastPathComponent];
+//	NSString *appPath = [fullPath stringByDeletingLastPathComponent];
 	// Search for path
+	return fullPath;
 //	return [NSString stringWithFormat:@"%u", appIconArray.count];
-	for (PSAppIcon *ic in appIconArray) {
+/*	for (PSAppIcon *ic in appIconArray) {
 		if (![ic.path compare:appPath])
 			return ic.icon;
 	}
@@ -112,6 +157,7 @@
 		appPath = [appPath stringByAppendingPathComponent:@"iTunesArtwork"];
 	}
 	return appPath;
+*/
 }
 
 + (UIImage *)getIconFromArray:(NSArray *)appIconArray forApp:(NSString *)fullPath size:(NSInteger)dim;
