f778216db0198d8593b35a081915d935fbdb49a2
Domo <domo@linux.com>
2015-04-09 02:50:02 +0300

    Новый bincopy.cmd

--- bincopy.cmd
+++ bincopy.cmd
@@ -1,3 +1,15 @@
-@"c:\Program Files (x86)\Applets\Putty\PSCP.EXE" -pw wsprintf root@192.168.0.107:/Applications/CocoaTop.app/CocoaTop CocoaTop
-@"c:\Program Files (x86)\Applets\Putty\PSCP.EXE" -pw wsprintf CocoaTop root@192.168.0.109:/Applications/CocoaTop.app/CocoaTop
-@pause
+@echo off
+echo 1. iPhone
+echo 2. iPad 1
+echo 3. iPad Black
+echo 4. iPad White
+CHOICE /C 1234 /N /M "Which will you choose? "
+IF ERRORLEVEL 1 SET DIP=192.168.0.109
+IF ERRORLEVEL 2 SET DIP=192.168.0.106 & SET SUFFIX=5
+IF ERRORLEVEL 3 SET DIP=192.168.0.104
+IF ERRORLEVEL 4 SET DIP=192.168.0.110
+set SIP=192.168.0.107
+echo Copying CocoaTop%SUFFIX% from %SIP% to %DIP%
+"c:\Program Files (x86)\Applets\Putty\PSCP.EXE" -pw wsprintf root@%SIP%:/Applications/CocoaTop.app/CocoaTop CocoaTop%SUFFIX%
+"c:\Program Files (x86)\Applets\Putty\PSCP.EXE" -pw wsprintf CocoaTop%SUFFIX% root@%DIP%:/Applications/CocoaTop.app/CocoaTop%SUFFIX%
+pause
