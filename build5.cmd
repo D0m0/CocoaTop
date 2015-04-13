6428179c4534aa8d7622f2c3f92a657cf3cccad1
Domo <domo@linux.com>
2015-04-13 21:46:32 +0300

    Сделал, чтобы программа не запускалась, если билд неудачен. Для этого нужно положить /bin/cp -> CocoaTop/sucp и дать ему sticky bit.

--- build5.cmd
+++ build5.cmd
@@ -1,5 +1,4 @@
 @set IP=192.168.0.106
 @"c:\Program Files (x86)\Applets\Putty\PSCP.EXE" -pw wsprintf src\* mobile@%IP%:/User/CocoaTop
-@"c:\Program Files (x86)\Applets\Putty\ansicon.exe" "c:\Program Files (x86)\Applets\Putty\PLINK.EXE" mobile@%IP% -pw wsprintf -batch <build.sh
-@"c:\Program Files (x86)\Applets\Putty\ansicon.exe" "c:\Program Files (x86)\Applets\Putty\PLINK.EXE" root@%IP% -pw wsprintf -batch <buildtask5.sh
+@"c:\Program Files (x86)\Applets\Putty\ansicon.exe" "c:\Program Files (x86)\Applets\Putty\PLINK.EXE" mobile@%IP% -pw wsprintf -batch <build5.sh
 @pause
--- build5.sh
+++ build5.sh
@@ -2,6 +2,6 @@ cd CocoaTop
 chmod 0555 postinst
 make
 if [[ $? != 0 ]]; then exit; fi
-./sucp -f obj/CocoaTop.app/CocoaTop /Applications/CocoaTop.app/CocoaTop
+./sucp -f obj/CocoaTop.app/CocoaTop /Applications/CocoaTop.app/CocoaTop5
 open ru.domo.CocoaTop
 exit
