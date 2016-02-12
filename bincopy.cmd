cb172aaad715289927913f18cee6171031c17062
Domo <domo@rambler.ru>
2016-02-13 01:20:47 +0300

    iPhone 4S

--- bincopy.cmd
+++ bincopy.cmd
@@ -3,7 +3,9 @@ echo 1. iPhone
 echo 2. iPad 1
 echo 3. iPad Black
 echo 4. iPad White
-CHOICE /C 1234 /N /M "Which will you choose? "
+echo 5. iPhone 4S
+CHOICE /C 12345 /N /M "Which will you choose? "
+IF ERRORLEVEL 5 SET DIP=192.168.0.111
 IF ERRORLEVEL 4 SET DIP=192.168.0.110
 IF ERRORLEVEL 3 SET DIP=192.168.0.104
 IF ERRORLEVEL 2 SET DIP=192.168.0.106 & SET SUFFIX=5
