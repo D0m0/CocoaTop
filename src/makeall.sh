e70be156a35c1b7d69bba0028636ca5da3ecedb5
Domo <domo@rambler.ru>
2016-02-22 11:50:07 +0300

    Новый скрипт для установки на девайсы

--- src/makeall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-make clean
-make
-make TARGET=iphone:clang:5.0 SCHEMA=five
-make TARGET=iphone:clang:6.0 SCHEMA=six
-make package
---
+++ src/makemenu.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+PS3="Please enter your choice: "
+options=("Clean & Make" "Make for 5 & 6" "iPhone 5c" "iPhone 4s" "iPad Black" "iPad White" "iPad 1" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+	"Clean & Make")
+	    make clean
+	    make
+	    ;;
+	"Make for 5 & 6")
+	    make TARGET=iphone:clang:5.0 SCHEMA=five
+	    make TARGET=iphone:clang:6.0 SCHEMA=six
+	    make package
+	    ;;
+	"iPhone 5c")
+	    make install THEOS_DEVICE_IP=192.168.0.109
+	    break
+	    ;;
+	"iPhone 4s")
+	    make install THEOS_DEVICE_IP=192.168.0.111
+	    break
+	    ;;
+	"iPad Black")
+	    make install THEOS_DEVICE_IP=192.168.0.104
+	    break
+	    ;;
+	"iPad White")
+	    make install THEOS_DEVICE_IP=192.168.0.110
+	    break
+	    ;;
+	"iPad 1")
+	    make install THEOS_DEVICE_IP=192.168.0.106
+	    break
+	    ;;
+	"Quit") break;;
+	*) echo invalid option;;
+    esac
+done
